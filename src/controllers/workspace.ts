c20912bfa3b447890aba806d1c9c6910958790fe
DagimN <dagmnebiat@gmail.com>
2023-03-26 16:00:26 +0300

    create and delete project route endpoints are connected with the github rest api

--- src/controllers/workspace.ts
+++ src/controllers/workspace.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from '../types/express';
 import asyncHandler from 'express-async-handler';
 import { Repository, User } from '../models/';
+import { Octokit } from 'octokit';
 /**
  * Get Projects
  * @route GET /api/v1/workspaces
@@ -28,21 +29,63 @@ const getProjects = asyncHandler(async (req: Request, res: Response) => {
 const createProject = asyncHandler(async (req: Request, res: Response) => {
   const { userId, projectName } = req.body as { userId: string; projectName: string };
   const user = await User.findById(userId);
+  const client = new Octokit({
+    auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+  });
   let errorMessage = 'User not found';
 
   if (user) {
     errorMessage = 'Project Creation Failed';
-    const repository = await Repository.create({ name: projectName, owner: userId });
+    // TODO: replace variables with actual values
+    const repoResponse = await client.request(`POST /orgs/${process.env.GITHUB_ORGANIZATION}/repos`, {
+      name: projectName,
+      homepage: 'https://github.com',
+      private: true,
+    });
 
-    if (repository) {
-      res.json({
-        message: `${projectName} is successfully created.`,
-      });
-      return;
+    if (repoResponse.status === 201) {
+      const repository = await Repository.create({ name: projectName, owner: userId });
+
+      if (repository) {
+        res.json({
+          message: `${projectName} is successfully created.`,
+        });
+        return;
+      }
     }
   }
   res.status(404);
   throw new Error(errorMessage);
 });
 
-export { getProjects, createProject };
+/**
+ * Delete Project
+ * @route GET /api/v1/workspaces
+ * @access Private
+ */
+const deleteProject = asyncHandler(async (req: Request, res: Response) => {
+  const { projectId, projectName } = req.body as { projectId: string; projectName: string };
+  const client = new Octokit({
+    auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+  });
+
+  const repoResponse = await client.request(`DELETE /repos/${process.env.GITHUB_ORGANIZATION}/${projectName}`);
+  let errorMessage = 'Failed Request';
+
+  if (repoResponse.status === 204) {
+    errorMessage = 'Project not found';
+    const project = await Repository.findByIdAndDelete(projectId);
+
+    if (project) {
+      res.json({
+        message: `${project.name} is successfully deleted.`,
+      });
+      return;
+    }
+  }
+
+  res.status(404);
+  throw new Error(errorMessage);
+});
+
+export { getProjects, createProject, deleteProject };
