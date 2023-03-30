5ab8ff5951d6525be27bddc513e9591343f81b8c
DagimN <dagmnebiat@gmail.com>
2023-03-30 20:51:35 +0300

    fixing merge conflict

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,4 @@
 import { User, Freelancer, Employer } from './User';
-import { User, Freelancer, Employeer } from './User';
 import Repository from './Repository';
 import Job from './Job';
 import Notification from './Notification';
