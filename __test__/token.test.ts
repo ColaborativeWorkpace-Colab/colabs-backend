f41ece4bc7129b3d012d1c5c5fb9190856a56c10
daniel <danielddemissie@gmail.com>
2023-04-06 17:01:55 +0300

    fix: test cases changed

--- __test__/token.test.ts
+++ __test__/token.test.ts
@@ -1,8 +1,8 @@
-import generateToken from '../src/utils/generateToken';
+import { verifyEmailFormat } from '../src/utils/mailFormats';
 
-describe('generateToken', () => {
-  it('should generate a valid token', () => {
-    const token = generateToken('1');
-    expect(token).toBeDefined();
+describe('General Test', () => {
+  it('should return verify email formate', () => {
+    const url = verifyEmailFormat('http://localhost:3000/verify');
+    expect(url).toBeDefined();
   });
 });
