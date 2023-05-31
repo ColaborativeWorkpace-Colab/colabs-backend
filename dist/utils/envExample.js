9ded7ba7531d285ee570d772c8120eee15c22278
Daniel Demelash <danielddemissie@gmail.com>
2023-05-31 10:21:24 +0300

    New prodction build generated

--- dist/utils/envExample.js
+++ dist/utils/envExample.js
@@ -5,11 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 function envExample() {
-    let envExample = fs_1.default.readFileSync('.env', 'utf8');
-    if (!envExample)
-        return;
-    envExample = envExample.replace(/=.*/g, '=env_value');
-    fs_1.default.writeFileSync('.env.example', envExample);
+    try {
+        let envExample = fs_1.default.readFileSync('.env', 'utf8');
+        envExample = envExample.replace(/=.*/g, '=env_value');
+        fs_1.default.writeFileSync('.env.example', envExample);
+    }
+    catch (error) {
+        if (error.code === 'ENOENT') {
+            console.log('.env file does not exist.');
+        }
+        else {
+            console.error('Error reading .env file:', error);
+        }
+    }
 }
 exports.default = envExample;
 //# sourceMappingURL=envExample.js.map
